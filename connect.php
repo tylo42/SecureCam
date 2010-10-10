0d6f6ed1d16eef98aca9e477160b52ea6fa76601
Tyler Hyndman <tyler.hyndman@gmail.com>
2010-10-10 17:34:34 -0500

    Add beginnings of a database class

--- connect.php
+++ connect.php
@@ -16,6 +16,8 @@
  * You should have received a copy of the GNU General Public License
  * along with SecureCam.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+// To be removed and replaced with securecam_database
 $dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = 'root';
@@ -25,4 +27,59 @@ $conn = mysql_connect($dbhost, $dbuser, $dbpass) or die
 
 $dbname = 'securecam';
 mysql_select_db($dbname);
+
+
+
+class securecam_database {
+   function __construct() {
+      $dbhost = 'localhost';
+      $dbuser = 'root';
+      $dbpass = 'root';
+      $dbname = 'securecam';
+
+      $this->conn = mysql_connect($dbhost, $dbuser, $dbpass) or die
+         ('Error connecting to mysql');
+
+      mysql_select_db($dbname);
+   }
+
+   function __destruct() {
+      mysql_close($this->conn);
+   }
+
+   /**
+    * $start_time       Unix timestamp for a date
+    * $end_time         Unix timestamp for a date
+    * $cameras          Array of cameras in use
+    */
+   public function search_videos($start_time, $end_time, $cameras, $page_num=1) {
+      if(!is_numeric($start_time) && !is_numeric($end_time) && empty($cameras) && !is_numeric($page_num) && $page_num < 1) {
+         return array();
+      }
+
+      $sql = "SELECT * FROM video WHERE $start_time < time AND time < $end_time";
+
+      foreach($cameras as $camera_num) {
+         if(is_numeric($camera_num)) {
+            $sql .= "AND camera_id = $camera_num";
+         } else {
+            return array();
+         }
+      }
+
+      $sql .= "LIMIT 20 OFFSET ".(($page_num-1)*20);
+
+      return $this->query_array($sql);
+   }
+
+   // HELPER FUNCTIONS
+   private function query_array($sql) {
+      $result = mysql_query($sql);
+      return mysql_fetch_array($result);
+   }
+
+   // DATA
+   private $conn;
+}
+
 ?>
