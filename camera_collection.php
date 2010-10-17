4b8ae0f1e06429d57fefe4daedca129473278753
Tyler Hyndman <tyler.hyndman@gmail.com>
2010-10-17 15:40:18 -0500

    Add GPL

--- camera_collection.php
+++ camera_collection.php
@@ -1,4 +1,21 @@
 <?php
+/* Copyright 2008, 2009, 2010 Tyler Hyndman
+ * 
+ * This file is part of SecureCam.
+ *
+ * SecureCam is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * SecureCam is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with SecureCam.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 class camera {
    function __construct($hostname, $port, $description) {
